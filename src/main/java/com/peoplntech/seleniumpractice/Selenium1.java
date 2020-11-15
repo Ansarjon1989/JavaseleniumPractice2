759b6d02b9fbc94ead507b91c4fb00389b712bbd
Ansar Rakhmankulov <ansarrakhmankulov@Ansars-MBP.lan1>
2020-11-14 22:38:40 -0500

    commit

--- src/main/java/com/peoplntech/seleniumpractice/Selenium1.java
+++ src/main/java/com/peoplntech/seleniumpractice/Selenium1.java
@@ -3,20 +3,26 @@ package com.peoplntech.seleniumpractice;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.sql.DriverManager;
-
-public class SeleniumPractice {
-
+public class Selenium1 {
 
     public static void main(String[] args) throws InterruptedException {
 
 
+        // set up data as chrome driver needs
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.chrome.com/");
 
+        //create object for chrome driver
+        WebDriver driver = new ChromeDriver();
+
+        //tell chrome driver to go to url....
+        driver.get("https://www.chrome.com/");
+
+        //wait 5 sec
         Thread.sleep(5000);
 
-        webDriver.close();
+        //close the chrome
+        driver.close();
+
     }
+
 }
